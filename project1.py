854f5de43bc62f3dd127eccafd7dd77326fdd349
ericareven <erica.reven@gmail.com>
2023-11-16 08:55:46 -0500

    project 1 complete

--- project1.py
+++ project1.py
@@ -24,4 +24,35 @@ while True:
     else:
         print("Invalid, try again.")
 
+max_score = 50
+player_scores = [0 for _ in range(players)] # loop number of players
 
+while max(player_scores) < max_score:
+
+    for player_id in range(players):
+        print("\nPlayer number", player_id +1, "turn has just started!")
+        print("Your total score is:", player_scores[player_id], "\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll (y)?")
+            if should_roll.lower() != "y": # convert input to lower case and check if it = y
+                break
+
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! Your turn is over")
+                current_score = 0
+                break # end turn
+            else: 
+                current_score += value
+                print("You rolled a: ", value)
+
+            print("Your score is: ", current_score)
+        
+        player_scores[player_id] += current_score
+        print("Your total score is: ", player_scores[player_id])
+    
+max_score = max(player_scores)
+winning_id = player_scores.index(max_score)
+print(f"Player number {winning_id + 1} is the winner with a score of: {max_score}") # +1 bc id starts at 0, and we want to display players 1-4, not 0-3
